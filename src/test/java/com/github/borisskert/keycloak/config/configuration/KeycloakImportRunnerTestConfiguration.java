3612644dfc27224726de173687a3bb13dc4b39fb
Boris Skert <boris.skert@gmail.com>
2020-03-01 22:07:48 +0100

    Introduce tests for import runner

--- src/test/java/com/github/borisskert/keycloak/config/configuration/KeycloakImportRunnerTestConfiguration.java
+++ src/test/java/com/github/borisskert/keycloak/config/configuration/KeycloakImportRunnerTestConfiguration.java
@@ -1,7 +1,5 @@
 package com.github.borisskert.keycloak.config.configuration;
 
-import com.github.borisskert.keycloak.config.KeycloakImportRunner;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -12,14 +10,11 @@ import org.springframework.web.client.RestTemplate;
 @ComponentScan(
         basePackages = {"com.github.borisskert.keycloak.config"}
 )
-@Profile("!KeycloakImportRunnerTest")
-public class TestConfiguration {
+@Profile("!IT")
+public class KeycloakImportRunnerTestConfiguration {
 
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
-
-    @MockBean
-    private KeycloakImportRunner keycloakImportRunner;
 }
