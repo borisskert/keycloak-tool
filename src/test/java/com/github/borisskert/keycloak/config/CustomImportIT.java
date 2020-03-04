434ab0cb3ca8cd06fc77b130606ecf94bd64d3c5
Boris Skert <boris.skert@gmail.com>
2020-03-04 18:47:09 +0100

    Clean up tests; Reformat and optimize imports; increase minimum value for test coverage

--- src/test/java/com/github/borisskert/keycloak/config/CustomImportIT.java
+++ src/test/java/com/github/borisskert/keycloak/config/CustomImportIT.java
@@ -9,7 +9,6 @@ import org.keycloak.representations.idm.RealmRepresentation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 
